61c389f635a4bdbcf90ea4a9dc1bb32af5dff693
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-23 19:04:14 +0100

    test update

--- src/test/java/com/rojatech/ramanujan/StringHelperTest.java
+++ src/test/java/com/rojatech/ramanujan/StringHelperTest.java
@@ -7,26 +7,31 @@ import org.junit.jupiter.api.Test;
 class StringHelperTest {
 
 	@Test
-	void test() {
+	void testTruncateAInFirstTwoCharacters() {
 		StringHelper sh = new StringHelper();
 		assertEquals("CD", sh.truncateAInFirstTwoCharacters("AACD"));
 		assertEquals("CAD", sh.truncateAInFirstTwoCharacters("ACAD"));
 		assertEquals("CDAB", sh.truncateAInFirstTwoCharacters("CDAB"));
 		assertEquals("CDAA", sh.truncateAInFirstTwoCharacters("CDAA"));
-		
+	}
+	
+	@Test
+	void testTruncateFirstTwoCharacters() {
+		StringHelper sh = new StringHelper();
 		assertEquals("CD", sh.truncateFirstTwoCharacters("AACD"));
 		assertEquals("AD", sh.truncateFirstTwoCharacters("ACAD"));
 		assertEquals("AB", sh.truncateFirstTwoCharacters("CDAB"));
 		assertEquals("AA", sh.truncateFirstTwoCharacters("CDAA"));
 		assertEquals("", sh.truncateFirstTwoCharacters("AA"));
+	}
 
+	@Test
+	void testareFirstTwoAndLastTwoCharactersTheSame() {
+		StringHelper sh = new StringHelper();
 		assertEquals(false, sh.areFirstTwoAndLastTwoCharactersTheSame("AACD"));
 		assertEquals(true, sh.areFirstTwoAndLastTwoCharactersTheSame("ABAB"));
 		assertEquals(true, sh.areFirstTwoAndLastTwoCharactersTheSame("CD"));
 		assertEquals(false, sh.areFirstTwoAndLastTwoCharactersTheSame("C"));
 		assertEquals(false, sh.areFirstTwoAndLastTwoCharactersTheSame(""));
-		
-		
 	}
-
 }
