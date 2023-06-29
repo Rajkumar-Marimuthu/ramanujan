b1b23d893fbccb8a89f64eff1650b218208a0452
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-29 17:08:03 +0100

    add test

---
+++ src/test/java/ListTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class ListTest {
+
+	@Test
+	public void testMockListSizeMethod() {
+		List listMock = mock(List.class);
+		when(listMock.size()).thenReturn(2);
+		assertEquals(2, listMock.size());
+	}
+
+	@Test
+	public void testMockListSizeMethodReturnMulipleValues() {
+		List listMock = mock(List.class);
+		when(listMock.size()).thenReturn(2).thenReturn(3);
+		assertEquals(2, listMock.size());
+		assertEquals(3, listMock.size());
+	}
+	
+	@Test
+	public void testMockListGetMethod() {
+		List listMock = mock(List.class);
+		when(listMock.get(0)).thenReturn("myname");
+		assertEquals("myname", listMock.get(0));
+	}
+}
