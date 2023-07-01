8ecdde36308f8446a8b8abb87120109b25e60006
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-07-01 16:27:08 +0100

    add test

--- src/test/java/com/rojatech/ramanujan/HamcrestMatchersTest.java
+++ src/test/java/com/rojatech/ramanujan/HamcrestMatchersTest.java
@@ -1,11 +1,14 @@
 package com.rojatech.ramanujan;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class HamcrestMatchersTest {
 	@Test
 	public void test() {
-		
+		List<Integer> scores = Arrays.asList(99,98,97,91,100);
 	}
 
 }
---
+++ src/test/java/com/rojatech/ramanujan/business/ToDoBusinessImplMockitoMockTest.java
@@ -0,0 +1,94 @@
+package com.rojatech.ramanujan.business;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.rojatech.ramanujan.data.api.ToDoService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ToDoBusinessImplMockitoMockTest {
+	
+	@Mock
+	ToDoService toDoServiceMock;
+
+	@Test
+	public void testRetriveToDoRelatedToString_usingMock() {
+		when(toDoServiceMock.retrieveToDos("dummy")).thenReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn Python"));
+		ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+		List<String> filteredToDos = toDoBusinessImpl.retriveToDoRelatedToString("dummy"); 
+		assertEquals(2, filteredToDos.size());
+	}
+	
+	@Test
+	public void testRetriveToDoRelatedToString_BDD() {
+		//Given
+		given(toDoServiceMock.retrieveToDos("dummy")).willReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn Python"));
+		ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+		
+		// When
+		List<String> filteredToDos = toDoBusinessImpl.retriveToDoRelatedToString("dummy"); 
+		// Then
+		
+		assertThat(filteredToDos.size(),is(2));
+	}
+	
+	@Test
+	public void testdeleteToDoNotRelatedToStringBDD() {
+		// Given
+		given(toDoServiceMock.retrieveToDos("dummy")).willReturn(Arrays.asList("Learn Spring MVC", "Learn Spring Boot", "Learn Python"));
+		ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+		
+		// When
+		toDoBusinessImpl.deleteToDoNotRelatedToString("dummy");
+		//List<String> filteredToDosList = toDoBusinessImpl.retriveToDoRelatedToString("dummy");
+		// Then
+		//assertThat(filteredToDosList.size(),is(2));
+		verify(toDoServiceMock, times(1)).deleteToDo("Learn Python");
+		verify(toDoServiceMock, never()).deleteToDo("Learn Spring MVC");
+		verify(toDoServiceMock, never()).deleteToDo("Learn Spring Boot");
+	}
+	
+	@Test
+	public void testdeleteToDoNotRelatedToStringBDD_argumentCapture() {
+		// Declare argument captor
+		ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+		
+		// Define Argument captor on specific method call
+		// Given
+		given(toDoServiceMock.retrieveToDos("dummy")).willReturn(Arrays.asList("Learn Spring MVC", "Learn Angular", "Learn Python"));
+		ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+		
+		// When
+		toDoBusinessImpl.deleteToDoNotRelatedToString("dummy");
+		//List<String> filteredToDosList = toDoBusinessImpl.retriveToDoRelatedToString("dummy");
+		// Then
+		//assertThat(filteredToDosList.size(),is(2));
+		then(toDoServiceMock).should(times(2)).deleteToDo(stringArgumentCaptor.capture());
+		assertThat(stringArgumentCaptor.getAllValues().size(), is(2));
+	}
+	
+	@Test
+	public void testRetriveToDoRelatedToString_usingMockEmptyList() {
+		when(toDoServiceMock.retrieveToDos("dummy")).thenReturn(Arrays.asList("Learn MVC","Learn Python", "Learn Cloud"));
+		ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+		List<String> filteredToDos = toDoBusinessImpl.retriveToDoRelatedToString("dummy"); 
+		assertEquals(0, filteredToDos.size());
+	}
+}
