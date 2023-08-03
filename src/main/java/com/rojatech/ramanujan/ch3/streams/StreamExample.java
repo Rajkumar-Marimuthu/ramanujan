17bf9df0d76cd9e926d27b5f05ddf09353c33544
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-08-03 21:48:48 +0100

    update

--- src/main/java/com/rojatech/ramanujan/ch3/streams/StreamExample.java
+++ src/main/java/com/rojatech/ramanujan/ch3/streams/StreamExample.java
@@ -3,6 +3,8 @@ package com.rojatech.ramanujan.ch3.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class StreamExample {
 
@@ -11,6 +13,32 @@ public class StreamExample {
 		String[] names = {"Pichai", "Elon", "Mike", "Amit", "Mary", "Laila", "Sairam"};
 		List<String> nameList = new ArrayList<>(Arrays.asList(names));
 		//List<String> nameList = Arrays.asList(names);
+		
+		// Create stream based on array
+		Arrays.stream(names).forEach(name -> System.out.println(name));
+		Optional<String> min = Arrays.stream(names).min((a,b) -> a.compareTo(b));
+		System.out.println(min);
+		
+		// Create stream based on list
+		Stream<String> listStream = nameList.stream();
+		boolean res = nameList.stream().allMatch(name -> !name.isEmpty());
+		System.out.println(res);
+		
+		// Create stream using generate method
+		Stream<Integer> generatedStream = Stream.generate(()->5);
+		
+		// create stream using iterate method
+		Stream<Integer> iteratedStream = Stream.iterate(1,x-> x+5);
+		
+		List<Integer> intList = Arrays.asList(4,3,7,4,2,1,9);		
+		System.out.println("Sum = " + intList.stream().reduce(0, (a,b)-> a+b));
+		
+		// flatMap
+		String[][] names2d = {names, names};
+		String[] names1d = Stream.of(names2d)
+				.flatMap(arr -> Stream.of(arr))
+				.peek(n -> System.out.println("peek " + n))
+				.toArray(size -> new String[size]);
 		
 		for (String name: names) {
 			if (name.startsWith("A") || name.startsWith("B")) {
@@ -35,5 +63,4 @@ public class StreamExample {
 		}
 		System.out.println(newNameList);
 	}
-
 }
